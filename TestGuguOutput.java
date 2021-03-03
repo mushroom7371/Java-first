f3c402f06e56758d168cedad4fbd89dfce212ed5
mushroom7371 <78005618+mushroom7371@users.noreply.github.com>
2021-03-03 14:05:46 +0900

    for 문을 이용한 구구단 예제를 객체 지향 프로그래밍 언어로 활용하기
    메서드를 이용하여 기능 분할 하여보고, 클래스 메서드로 나눠 구현하는 연습

---
+++ TestGuguOutput.java
@@ -0,0 +1,18 @@
+
+class TestGuguOutput {
+
+	public static void main(String[] args) {
+		guguMethod();
+	}
+
+	static void guguMethod() {
+		for(int i = 2; i <= 9; i++) {
+			for(int j = 1; j <=9; j++) 
+				guguOutput(i,j);
+		}
+	}
+	
+	static void guguOutput(int i, int j) {
+		System.out.printf("%d X %d = %d%n", i, j, i*j);
+	}
+}
