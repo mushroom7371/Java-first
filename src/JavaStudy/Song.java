175a7b12f342a14d499eedf0ce09eb4b0ef6bb04
JuneHwa_Park <wnsghk6670@naver.com>
2021-04-10 21:13:57 +0900

    자바 기초 복습 클래스의 필드 선언과 타 클래스에서의 사용

---
+++ src/JavaStudy/Song.java
@@ -0,0 +1,10 @@
+package JavaStudy;
+
+public class Song {
+	
+	String songTitle;	//클래스의 필드를 선언함
+	String singer;		//각각 String 타입, int 타입의 변수를 선언하나 초기화는 하지 않음
+	String albumName;
+	int trackNumber;
+	
+}
