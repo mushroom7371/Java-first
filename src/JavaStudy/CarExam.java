461ab2615dd39797298aa27b9f4501be75063e8d
JuneHwa_Park <wnsghk6670@naver.com>
2021-04-11 18:52:22 +0900

    자바 기초 복습 생성자, 오버로딩, this 와 관련한 이해

---
+++ src/JavaStudy/CarExam.java
@@ -0,0 +1,11 @@
+package JavaStudy;
+
+public class CarExam {
+	
+	Car car = new Car();
+	Car car1 = new Car("소방차", 1111);
+	Car car2 = new Car("경찰차");
+	
+	// 각각 Car 클래스에서 선언한 생성자를 받아오는 것을 확인 할 수있다.
+	// 만약 선언되지 않은 형태로 매개변수를 입력하면 오류가 발생한다.
+}
