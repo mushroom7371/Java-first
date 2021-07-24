cab3e95c0df22602954dc6491cbb5e73bd96dc6e
juna desktop <wnsghk6670@naver.com>
2021-07-24 21:36:38 +0900

    자바 클래스 공부 StringBuffer, StringBuilder 내부메서드 사용법 공부

--- src/Study/StringBuffer_Builder.java
+++ src/Study/StringBuffer_Builder.java
@@ -16,5 +16,34 @@ public class StringBuffer_Builder {
 		System.out.println(sb);
 		//버퍼와 빌더는 가변성을 가졌기 때문에 append() delete() 등과 같은 메서드로 동일 객체내에서 문자열을 변경하는 것이 가능하다.
 		
+		String s = "abcdefg";
+        StringBuffer sb1 = new StringBuffer(s); // String -> StringBuffer
+		
+        System.out.println("처음 상태 : " + sb1); //처음상태 : abcdefg
+        System.out.println("문자열 String 변환 : " + sb1.toString()); //String 변환하기
+        System.out.println("문자열 추출 : " + sb1.substring(2,4)); //문자열 추출하기
+        System.out.println("문자열 추가 : " + sb1.insert(2,"추가")); //문자열 추가하기
+        System.out.println("문자열 삭제 : " + sb1.delete(2,4)); //문자열 삭제하기
+        System.out.println("문자열 연결 : " + sb1.append("hijk")); //문자열 붙이기
+        System.out.println("문자열의 길이 : " + sb1.length()); //문자열의 길이구하기
+        System.out.println("용량의 크기 : " + sb1.capacity()); //용량의 크기 구하기
+        System.out.println("문자열 역순 변경 : " + sb1.reverse()); //문자열 뒤집기
+        System.out.println("마지막 상태 : " + sb1); //마지막상태 : kjihgfedcba
+        
+        String s2 = "abcdefg";
+        StringBuilder sb2 = new StringBuilder(s2); // String -> StringBuilder
+		
+        System.out.println("처음 상태 : " + sb2); //처음상태 : abcdefg
+        System.out.println("문자열 String 변환 : " + sb2.toString()); //String 변환하기
+        System.out.println("문자열 추출 : " + sb2.substring(2,4)); //문자열 추출하기
+        System.out.println("문자열 추가 : " + sb2.insert(2,"추가")); //문자열 추가하기
+        System.out.println("문자열 삭제 : " + sb2.delete(2,4)); //문자열 삭제하기
+        System.out.println("문자열 연결 : " + sb2.append("hijk")); //문자열 붙이기
+        System.out.println("문자열의 길이 : " + sb2.length()); //문자열의 길이구하기
+        System.out.println("용량의 크기 : " + sb2.capacity()); //용량의 크기 구하기
+        System.out.println("문자열 역순 변경 : " + sb2.reverse()); //문자열 뒤집기
+        System.out.println("마지막 상태 : " + sb2); //마지막상태 : kjihgfedcba
+		
+		
 	}
 }
