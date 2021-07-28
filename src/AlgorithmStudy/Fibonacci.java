cbb46bbb8c328545aa6ccbd129d324bb41e5759d
JuneHwa_Park <wnsghk6670@naver.com>
2021-07-28 12:39:01 +0900

    자바 알고리즘 공부 반복문/재귀함수를 이용한 피보나치 수열 항 구하기

--- src/AlgorithmStudy/Fibonacci.java
+++ src/AlgorithmStudy/Fibonacci.java
@@ -11,6 +11,8 @@ public class Fibonacci {
 		int num = sc.nextInt();
 		//원하는 피보나치 수열의 항을 입력받음
 		
+		
+		System.out.println("반복분을 이용한 피보나치 n항 구하기");
 		int su1, su2, sum;
 		//수열을 구하기 위한 변수 선언
 		
@@ -20,10 +22,27 @@ public class Fibonacci {
 		
 		for(int i = 0; i < num; i++) {
 			System.out.print(sum + " ");
+			
 			sum = su1 + su2;
 			su1 = su2;
 			su2 = sum;
 			//첫번째 항과 두번째항을 고정한 후 밀어내기 식으로 피보나치 수열을 구한다.
+		}
+		
+		System.out.println();
+		System.out.println("재귀함수를 이용한 피보나치 n항 구하기");
+		
+		System.out.println(fibo(num));
+		
+	}
+	
+	static int fibo(int n) {
+		if(n == 0) {
+			return 0;
+		}else if(n == 1) {
+			return 1;
+		}else {
+			return fibo(n-1) + fibo(n-2);
 		}
 	}
 }
