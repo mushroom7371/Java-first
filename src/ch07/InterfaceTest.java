3f99c9da97af59ab4ce362747a8ceb8cee2d44ff
juna desktop <wnsghk6670@naver.com>
2021-08-07 16:02:33 +0900

    자바 개념 복습 인터페이스 타입의 참조변수를 매개변수로 가지는 메서드 및 인터페이스 구현클래스에 대한 이해

--- src/ch07/InterfaceTest.java
+++ src/ch07/InterfaceTest.java
@@ -4,12 +4,14 @@ public class InterfaceTest {
 	public static void main(String [] args) {
 		A a = new A();
 		a.autoPlay(new B());	//B의 메서드를 사용하기위해 객체를 생성해주어야됨
-		a.autoPlay(new C());
+		a.autoPlay(new C());	//하지만 생성하고 메서드를 이용한 후 참조를 할 수 없어 가비지 컬렉터로 인하여 사라짐
+								//두 메서드는 매개변수로 인터페이스를 구현한 클래스의 인스턴스를 받음으로써 메서드가 실행된다.
 	}
 }
 
 class A {
-	void autoPlay(I i) {	//인터페이스 I 타입의 i를 매개변수로 가진다
+	void autoPlay(I i) {	//인터페이스 I 타입의 참조변수 i를 매개변수로 가진다
+							//매개변수를 통해서 인터페이스를 구현한 클래스의 인스턴스를 동적으로 제공 받아야 함
 		i.play();
 	}
 }
@@ -20,12 +22,12 @@ interface I{	//인터페이스 정의
 
 class B implements I{
 	public void play() {
-		System.out.println("Play in B class"); //인터페이스 I를 구현한 B클래스에서 사용할 play 메서드
+		System.out.println("Play in B class"); //인터페이스 I를 구현한 B클래스에서 사용할 play 메서드, 오버라이딩 하였다.
 	}
 }
 
 class C implements I{
 	public void play() {
-		System.out.println("Play in C class"); //인터페이스 I를 구현한 C클래스에서 사용할 play 메서드
+		System.out.println("Play in C class"); //인터페이스 I를 구현한 C클래스에서 사용할 play 메서드, 오버라이딩 하였다.
 	}
 }
