b9f8440dd358e5b69a3a61ff5b2bdbbe50cdddaf
juna desktop <wnsghk6670@naver.com>
2021-08-11 21:14:41 +0900

    자바 개념 복습 다른 클래스에서 오버라이딩된 toString() 메서드 확인

---
+++ src/ch09/ToStringTest.java
@@ -0,0 +1,17 @@
+package ch09;
+
+import java.util.Date;
+
+public class ToStringTest {
+	public static void main(String[] args) {
+		String str = new String("KOREA");
+		Date today = new Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		//String 클래스에서의 toString() 메서드는 Object 클래스의 toString() 메서드를 오버라이딩(재정의) 하였다.
+		System.out.println(today);
+		System.out.println(today.toString());
+		//마찬가지로 Date 클래스 또한 toString() 메서드를 재정의 하였다.
+	}
+}
