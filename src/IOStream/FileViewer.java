f5206c52ae79f1c380a8cacf4cb243128017ceff
juna desktop <wnsghk6670@naver.com>
2021-08-22 13:46:59 +0900

    자바 입출력 공부 Viewer 예제 범위오류 발생 찾아볼것

---
+++ src/IOStream/FileViewer.java
@@ -0,0 +1,15 @@
+package IOStream;
+
+import java.io.*;
+
+class FileViewer {
+	public static void main(String args[]) throws IOException{
+		FileInputStream fis = new FileInputStream(args[0]);
+		int data = 0;
+
+		while((data=fis.read())!=-1) {
+			char c = (char)data;
+			System.out.print(c);
+		}
+	} 
+}
