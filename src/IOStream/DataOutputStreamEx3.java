a414f1a9cbb80c06315b5139d4ad652fb5d96a2a
juna desktop <wnsghk6670@naver.com>
2021-08-28 18:57:48 +0900

    자바 입출력 공부 FileOutputStream과 DataOutputStream을 이용한 score.dat 파일 생성

---
+++ src/IOStream/DataOutputStreamEx3.java
@@ -0,0 +1,24 @@
+package IOStream;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataOutputStreamEx3 {
+	public static void main(String[] args) {
+		int [] score = {100, 90, 95, 85, 50};
+		//기본형 배열선언 및 초기화
+		try {
+			FileOutputStream fos = new FileOutputStream("score.dat");
+			//FileOutputStream을 통한 score.dat 파일 생성
+			DataOutputStream dos = new DataOutputStream(fos);
+			
+			for(int i = 0; i < score.length; i++) {
+				dos.writeInt(score[i]);
+			}
+			//DataOutputStream을 이용하여 배열의 길이만큼 객체에 int값을 저장한다.
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
