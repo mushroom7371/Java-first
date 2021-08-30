e9910aae3ab76eaf3d5294ad4ea96ef7b572d334
JuneHwa_Park <wnsghk6670@naver.com>
2021-08-30 16:48:56 +0900

    자바 컬렉션프레임워크 공부 ListIterator에 대한 이해내용 추가

--- src/CollectionsFramework/ListIteratorEx1.java
+++ src/CollectionsFramework/ListIteratorEx1.java
@@ -14,9 +14,9 @@ public class ListIteratorEx1 {
 		list.add("4");
 		list.add("5");
 		
-		ListIterator it = list.listIterator();	//ListIterator 타입의 참조변수 it을 선언하고 listIterator()메서드를 호출하여 리턴값을 저장함
-		
-		while(it.hasNext()) {	//it가 가리키는 인스턴스에 다음 데이터가 있는 경우
+		ListIterator it = list.listIterator();	//ListIterator 타입의 참조변수 it을 선언하고 ArrayList객체가 가지고 있는 listIterator()메서드를 호출하여 리턴값을 저장함
+												//이때 참조변수의 타입을 ListIterator를 사용한 이유는 listIterator()메서드 처리결과로 반환된 값이 ListIterator타입이기 때문이다.
+		while(it.hasNext()) {	//it가 가리키는 인스턴스에 다음 데이터가 있는 경우, 반복의 범위가 정해져있지 않기에 false값이 될 경우 출력하지 않는다.
 			System.out.println(it.next());	//그 데이터를 불러와서 출력한다.
 		}
 		
