655e90e5a0969b8415dc30387cadf5538b96c837
juna desktop <wnsghk6670@naver.com>
2021-08-31 19:23:36 +0900

    자바 컬렉션프레임워크 공부 HashMap 복습 및 이해내용 추가

--- src/CollectionsFramework/HashMapEx2.java
+++ src/CollectionsFramework/HashMapEx2.java
@@ -19,22 +19,24 @@ public class HashMapEx2 {
 		
 		Set set = map.entrySet();	//Set 타입의 참조변수 set을 선언하고 map의 데이터를 엔트리 형태(키와 값을 결합)로 저장한다.
 		Iterator it = set.iterator();	//set의 iterator()메서드를 호출하여 리턴받은 값을 Iterator 타입의 참조변수 it에 저장한다.
+										//Iterator를 사용하여 값을 읽어들일 준비를 한다.
 
 		while(it.hasNext()) { //참조변수 it이 가리키는 인스턴스에 다음 값이 있을때까지 반복
 			Map.Entry e = (Map.Entry)it.next();	//Map.Entry타입의 참조변수에 it.next()를 형변환 하여 저장한다.
 			System.out.println("이름 : " + e.getKey() + ", 점수 : " + e.getValue());
+			//entry 형태로 저장된 값을 출력
 		}
 		
-		set = map.keySet();	//참조변수 set에 HashMap에 저장된 모든 키가 저장된 Set을 반환한다.
+		set = map.keySet();	//참조변수 set에 kdySet()메서드로 반환된 모든 키 값을 반환한다. 리턴 타입이 Set이기에 참조변수의 타입과 일치한다.
 		System.out.println("참가자 명단 = " + set);
 		
-		Collection values = map.values();	//Collection 타입의 참조변수에 HashMap에 저장된 모든 값을 Collection 형태로 반환
+		Collection values = map.values();	//Collection 타입의 참조변수에 참조변수 map이 가리키는 HashMap 객체에 저장된 모든 값을 Collection 형태로 반환
 		it = values.iterator();	//Iterator는 재활용이 안되므로 다시 값을 저장해 둔다.
 		
 		int total = 0;
 		
 		while(it.hasNext()) {	//it에 다음값이 있을때 까지 반복
-			Integer i = (Integer)it.next();	//it의 다음데이터를 Integer 타입으로 형변환 하여 변수i에 저장
+			Integer i = (Integer)it.next();	//it의 다음 데이터를 Integer 타입으로 형변환 하여 변수i에 저장
 			total = total + i.intValue();	//total 변수에 i의 값을 저장한다.
 		}
 		
