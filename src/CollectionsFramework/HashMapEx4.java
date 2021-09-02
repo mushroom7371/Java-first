63933de3caa4ee897611aa0552f2e0bb495845e8
juna desktop <wnsghk6670@naver.com>
2021-09-02 19:01:19 +0900

    자바 컬렉션프레임워크 공부 HashMap을 이용한 문자열 빈도 구하기

--- src/CollectionsFramework/HashMapEx4.java
+++ src/CollectionsFramework/HashMapEx4.java
@@ -7,21 +7,23 @@ import java.util.Map;
 class HashMapEx4 {
 	public static void main(String[] args) {
 		String [] data = {"A", "K", "A", "K", "D", "K", "A", "K", "K", "K", "Z", "D"};
+		//String 타입의 데이터를 저장하는 배열을 생성하고 참조변수에 주소값을 담았다.
 		
 		HashMap map = new HashMap();
+		//HashMap 객체를 생성
 		
-		for(int i = 0; i < data.length; i++) {
-			if(map.containsKey(data[i])) {
-				Integer value = (Integer)map.get(data[i]);
-				map.put(data[i], new Integer(value.intValue() + 1));
+		for(int i = 0; i < data.length; i++) {	//참조변수 data가 가리키는 배열의 길이만큼 반복
+			if(map.containsKey(data[i])) {	//HashMap객체가 data가 가리키는 배열의 각번지수의 데이터를 가지고 있다면
+				Integer value = (Integer)map.get(data[i]);	//HashMap객체가 가지고있는 해당 데이터를 얻어와서 형변환을 통해 타입을 일치 시킨 후, 변수 value에 그 값을 저장한다.
+				map.put(data[i], new Integer(value.intValue() + 1));	//HashMap객체에 해당 데이터의 값의 value를 1증가 시킨 후 저장한다.
 			}else {
-				map.put(data[i], new Integer(1));
+				map.put(data[i], new Integer(1));	//가지고 있지 않다면 1로 지정한다.
 			}
 		}
 		
-		Iterator it = map.entrySet().iterator();
+		Iterator it = map.entrySet().iterator();	//HashMap의 데이터를 읽어오기위한 준비.
 		
-		while(it.hasNext()){
+		while(it.hasNext()){	//하단의 메서드를 통하여 출력을 실시한다.
 			Map.Entry entry = (Map.Entry)it.next();
 			int value = ((Integer)entry.getValue()).intValue();
 			System.out.println(entry.getKey() + " : " + printBar('#', value) + " " + value);
@@ -29,7 +31,7 @@ class HashMapEx4 {
 		}
 	}
 	
-	public static String printBar(char ch, int value) {
+	public static String printBar(char ch, int value) {	//그래프 형식으로 출력하기 위한 메서드
 		char[] bar = new char[value];
 		
 		for(int i = 0; i < bar.length; i++)
