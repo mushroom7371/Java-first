b3d230ad5eb231154c28f84a49d0816d867e544c
JuneHwa_Park <wnsghk6670@naver.com>
2021-09-07 19:10:53 +0900

    자바 지네릭스 공부 와일드 카드를 통한 타입 변수 활용

--- src/Generics/FruitBoxEx3.java
+++ src/Generics/FruitBoxEx3.java
@@ -31,22 +31,25 @@ class Juicer{
 
 class FruitBoxEx3 {
 	public static void main(String[] args) {
-		FruitBox<Fruit1> fruitBox = new FruitBox<Fruit1>();
-		FruitBox<Apple1> appleBox = new FruitBox<Apple1>();
+		FruitBox<Fruit1> fruitBox = new FruitBox<Fruit1>();	//FruitBox 객체를 생성하지만 여기에는 Fruit1 타입의 객체만을 담을 수있다.
+		FruitBox<Apple1> appleBox = new FruitBox<Apple1>();	//FruitBox 객체를 생성하지만 여기에는 Apple1 타입의 객체만을 담을 수있다.
 		
-		fruitBox.add(new Apple1());
+		fruitBox.add(new Apple1());	//fruitBox가 가리키는 객체에는 Fruit1클래스를 상속받은 Apple1, Grape1 객체 모두 담을 수 있다.
 		fruitBox.add(new Grape1());
-		appleBox.add(new Apple1());
+		appleBox.add(new Apple1());	//appleBox가 가리키는 객체에는 Apple1 객체만 담을 수 있다.
 		appleBox.add(new Apple1());
 		
 		System.out.println(Juicer.makeJuice(fruitBox));
 		System.out.println(Juicer.makeJuice(appleBox));
+		//static 메서드를 호출하여 해당 box에 담긴 내용을 출력한다.
+		//순차적으로 보면 for문으로 Fruit1 타입의 참조변수 f에 해당 인자값으로 전달 받은 box의 객체들이 ArrayList객체로 담기게 되고,
+		//반복마다 String tmp에 toString의 결과로 문자열이 더해 진다. 끝으로 Juice(tmp)를 통하여 tmp에 저장된 문자열에 Juice문자열이 더해진 결과가 반환된다.
 		
 	}
 }
 
 class FruitBox<T extends Fruit1> extends Box1<T>{
-	
+	//과일 박스 클래스, Fruit1 클래스를 상속받는 타입변수 T를 지정하였고 Box1 클래스를 상속받는다.
 }
 
 class Box1<T>{
