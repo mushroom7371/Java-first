fe8a1b6707dbe92f2903e6e45ea80e5f96299fd4
juna desktop <wnsghk6670@naver.com>
2021-10-08 23:09:41 +0900

    정보처리 시험대비 알고리즘 공부 txt파일을 읽어와 출장비를 화퍠종류별로 정리

--- src/certificate/Section050_money.java
+++ src/certificate/Section050_money.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 class Section050_money {
-	//우리나라 화폐의 종류별 매수 계산... 커밋로그 꼬임 롤백 어케하징...
+	//우리나라 화폐의 종류별 매수 계산... 커밋로그 꼬임 롤백 어케하징... 복구 완료 git 너무 어려움
 	
 	//static 상수와 내부 클래스를 선언하여 객체 생성없이 클래스가 메모리에 적재되면 사용가능(단, 내부 클래스는 클래스이기 때문에 객체를 만들고 참조변수로 가리켜야됨)
 	static final int LENGTH = 10;	//상수로 선언하여 변하지 않도록 설정했다.
