1ff5b4e55ab1b1af1304f722a9a8330730948a6e
juna <wnsghk6670@naver.com>
2021-11-26 13:39:41 +0900

    프로그래머스 level1 숫자로 이루어진 문자열을 정수값으로 반환하기

---
+++ src/AlgorithmStudy/StringToInt.java
@@ -0,0 +1,18 @@
+package AlgorithmStudy;
+
+//문자열s를 숫자로 변환한 결과를 반환하는 함수, solution 완성
+/*제한 조건
+s의 길이는 1 이상 5이하입니다.
+s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+s는 부호와 숫자로만 이루어져있습니다.
+s는 "0"으로 시작하지 않습니다.*/
+
+public class StringToInt {
+	public int solution(String s) {
+		//solution() 메서드를 호출 할 때 String 타입의 참조변수 s를 인자로 받는다.
+		return Integer.parseInt(s);
+		
+		//Integer 클래스의 static 메서드인 parseInt()를 사용한다.
+		//해당 메서드는 숫자형의 문자열을 인자값으로 받으면 해당 값을 10진수의 Integer형으로 반환한다.
+	}
+}
