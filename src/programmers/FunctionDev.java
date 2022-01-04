15f33555e3a6011e5ff0995057e5e6b73b08db5c
juna <wnsghk6670@naver.com>
2022-01-04 19:21:40 +0900

    프로그래머스 level2 기능개발

--- src/programmers/FunctionDev.java
+++ src/programmers/FunctionDev.java
@@ -7,27 +7,29 @@ class FunctionDev {
 		int day = 0;	//tmp에 적용할 배포일 수
 		
 		for(int i = 0; i < progresses.length; i++) {	//progresses가 가리키는 배열의 크기만큼 1씩 증가하며 반복
-			while(progresses[i] + (speeds[i] * day) < 100) {	//작업 진행도가 100보다 작다면
+			while(progresses[i] + (speeds[i] * day) < 100) {	//해당 프로세스의 진행가 100이 넘을때의 day를 구함
 				day++;	//일수가 늘어난다.
 			}
 			
-			tmp[day]++;
+			tmp[day]++;	//day 번지의 데이터를 1 증가 시킨다. (day번지에 배포가 가능한 프로세스가 늘어난다.)
 		}
 		
 		int count = 0;
 		
+		//tmp에 들어간 배열의 길이를 구하기 위한 코드
+		//tmp는 초기에 0으로 선언 되어 있으므로 0이 아닌 값만 적용
 		for(int n : tmp) {
 			if(n != 0) {
-				count++;
+				count++;	//n이 0이 아니라면 해당 day에 배포가 가능한 작업이 있으므로 count가 증가한다.
 			}
 		}
 		
-		int [] answer = new int[count];
+		int [] answer = new int[count];	//배포가 가능한 day 만큼의 길이를 가진 배열을 생성
 		
-		count = 0;
+		count = 0;	//초기화
 		for(int n : tmp) {
 			if(n != 0) {
-				answer[count++] = n;
+				answer[count++] = n;	//tmp에는 배포가 가능한 프로세스의 수가 담겨있다. 이를 answer에 넣는다.
 			}
 		}
 		
