c196238e12f90071563b6fbcc22d02d32504e51d
juna <wnsghk6670@naver.com>
2022-01-17 18:19:06 +0900

    프로그래머스 level2 소수 찾기

---
+++ src/programmers/FindPrimeNum.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class FindPrimeNum {
+	boolean [] visit;
+	char [] arr;
+	Set<Integer> set = new HashSet<Integer>();
+	
+	public boolean isPrime() {
+		
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	static int solution(String numbers) {
+		int answer = 0;
+		return answer;
+    }
+}
