01e1da004ebd72d8319c78dcd7e358690c53c267
juna <wnsghk6670@naver.com>
2022-01-18 18:13:14 +0900

    프로그래머스 level2 소수찾기(깊은 탐색)

--- src/programmers/FindPrimeNum.java
+++ src/programmers/FindPrimeNum.java
@@ -8,19 +8,56 @@ class FindPrimeNum {
 	char [] arr;
 	Set<Integer> set = new HashSet<Integer>();
 	
-	public boolean isPrime() {
+	public boolean isPrime(int n) {
+		if(n == 1 || n == 0) {
+			return false;
+		}
 		
+		for(int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
 		
 		return true;
 	}
 	
+	public int charsToInt(char [] arr, int length) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < length; i++) {
+			sb.append(arr[i]);
+		}
+		
+		return Integer.parseInt(sb.toString());
+	}
 	
+	public void dfs(int level, int length, String numbers) {
+		if(level == length) {
+			int num = charsToInt(arr, length);
+			if(isPrime(num)) {
+				set.add(num);
+			}
+		}else {
+			for(int i = 0; i < numbers.length(); i++) {
+				if(!visit[i]) {
+					visit[i] = true;
+					arr[level] = numbers.charAt(i);
+					dfs(level + 1, length, numbers);
+					visit[i] = false;
+				}
+			}
+		}
+	}
 	
-	
-	
-	
-	static int solution(String numbers) {
-		int answer = 0;
-		return answer;
+	public int solution(String numbers) {
+		visit = new boolean[numbers.length()];
+		arr = new char[numbers.length()];
+		
+		for(int i = 1; i <= numbers.length(); i++) {
+			dfs(0, i, numbers);
+		}
+		
+		return set.size();
     }
 }
