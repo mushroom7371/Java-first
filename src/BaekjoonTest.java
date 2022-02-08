99077297f277e885d4ad96e71f5d0e395c8a9303
juna <wnsghk6670@naver.com>
2022-02-08 18:20:49 +0900

    프로그래머스 level2 숫자의 표현(미완)

---
+++ src/BaekjoonTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekjoonTest {
+	public static void main(String[] args) {
+		
+	}
+	
+	public class Main {
+		public static void main(String[] args){
+			
+		}
+	}
+}
---
+++ src/programmers/ExpressionOfNumber.java
@@ -0,0 +1,20 @@
+package programmers;
+
+//숫자의 표현
+public class ExpressionOfNumber {
+	static int solution(int n) {
+		int answer = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		int n = 15;
+		
+		solution(n);
+	}
+}
