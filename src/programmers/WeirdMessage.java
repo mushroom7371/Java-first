240792c47b996ff514d86fc00650d95ec684dd00
juna <wnsghk6670@naver.com>
2022-02-17 09:43:44 +0900

    프로그래머스 level1 이상한 문자 만들기

--- src/programmers/WeirdMessage.java
+++ src/programmers/WeirdMessage.java
@@ -2,37 +2,26 @@ package programmers;
 
 //이상한 문자 만들기
 class WeirdMessage {
-	static String solution(String s) {
+	private String solution(String s) {
 		String answer = "";
-		String [] str = s.split(" ");
+		String [] str = s.split("");	//주어진 문자열을 한글자씩 나누어 배열에 저장한다.
+		int idx = 0;	//대,소문자 기준이 될 인덱스
 		
-		for(int i = 0; i < str.length; i++) {
-			String temp = "";
-
-			for(int j = 0; j < str[i].length(); j++) {
-				
-				if(j % 2 == 0) {
-					temp += Character.toUpperCase(str[i].charAt(j));
-				}else {
-					temp += str[i].charAt(j);
+		for(int i = 0; i < str.length; i++) {	//한글자 씩 나뉘어 담김 배열의 길이만큼 반복
+			if((" ").equals(str[i])) {	//i번지의 데이터가 띄어쓰기 라면
+				idx = 0;	//인덱스를 0으로 초기화 한다.
+			}else {
+				if(idx % 2 == 0) {	//인덱스가 짝수라면
+					idx++;	//인덱스를 1증가 시킴
+					str[i] = str[i].toUpperCase();	//해당 글자를 대문자로 변경
+				}else {	//인덱스가 홀수라면
+					idx++;	//인덱스를 1증가 시킴
+					str[i] = str[i].toLowerCase();	//해당 글자를 소문자로 변경
 				}
 			}
-			
-			str[i] = temp;
-			
-			if(i != str.length-1) {
-				answer += str[i] + " ";
-			}else {
-				answer += str[i];
-			}
+			answer += str[i];	//반복마다 answer에 해당 글자를 담아둔다
 		}
 		
 		return answer;
-	}
-	
-	public static void main(String[] args) {
-		String s = "try hello world";
-		
-		solution(s);
 	}
 }
