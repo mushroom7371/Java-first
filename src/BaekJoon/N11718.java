4a4a0be297df002b2a7446b5a2ca6f74a945f3b0
juna desktop <wnsghk6670@naver.com>
2022-05-23 18:51:45 +0900

    백준 11718번 그대로 출력하기

---
+++ src/BaekJoon/N11718.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+//그대로 출력하기
+public class N11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+        sc.close();
+
+    }
+}
