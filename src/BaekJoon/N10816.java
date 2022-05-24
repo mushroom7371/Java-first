168c03c34039abf26cf90d4876d41cdd69e524e2
juna <wnsghk6670@naver.com>
2022-05-24 11:49:38 +0900

    백준 10816번 숫자 카드 2(80%에서 틀림. 반례를 못찾겠음)

--- src/BaekJoon/N10816.java
+++ src/BaekJoon/N10816.java
@@ -1,6 +1,9 @@
 package BaekJoon;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 //숫자 카드 2
@@ -9,34 +12,51 @@ public class N10816 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+
         int numberOfSanggeunsCards = Integer.parseInt(br.readLine());
+        int [] cardTemp = new int[numberOfSanggeunsCards];
+        Map<Integer, Integer> sangGeunCards = new HashMap<>();
         st = new StringTokenizer(br.readLine());
-        int [] sangGeunCards = new int[numberOfSanggeunsCards];
+        int count = 1;
 
         for(int i = 0; i < numberOfSanggeunsCards; i++){
-            sangGeunCards[i] = Integer.parseInt(st.nextToken());
+            cardTemp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cardTemp);
+
+        for(int i = 0; i < numberOfSanggeunsCards - 1; i++){
+            if(i < numberOfSanggeunsCards - 2){
+                if(cardTemp[i] == cardTemp[i+1]){
+                    count++;
+                }else{
+                    sangGeunCards.put(cardTemp[i], count);
+                    count = 1;
+                }
+            }else{
+                if(cardTemp[i] == cardTemp[numberOfSanggeunsCards -1]){
+                    count++;
+                    sangGeunCards.put(cardTemp[i], count);
+                }else{
+                    sangGeunCards.put(cardTemp[i], count);
+                    sangGeunCards.put(cardTemp[numberOfSanggeunsCards -1], 0);
+                }
+            }
         }
 
         int numberOfTargetCards = Integer.parseInt(br.readLine());
-        int [] targetCards = new int[numberOfTargetCards];
-
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < numberOfTargetCards; i++){
-            targetCards[i] = Integer.parseInt(st.nextToken());
-        }
+        Map<Integer, Integer> targetCards = new HashMap<>();
 
         for(int i = 0; i < numberOfTargetCards; i++){
-            int count = 0;
-            for(int j = 0; j < numberOfSanggeunsCards; j++){
-                if(targetCards[i] == sangGeunCards[j]){
-                    count++;
-                }
+            int temp = Integer.parseInt(st.nextToken());
+            targetCards.put(temp, 0);
+
+            if(sangGeunCards.containsKey(temp)){
+                targetCards.put(temp, sangGeunCards.get(temp));
             }
-            targetCards[i] = count;
-        }
 
-        for(int i = 0; i < numberOfTargetCards; i++){
-            bw.write(targetCards[i] + " ");
+            bw.write(targetCards.get(temp) + " ");
         }
 
         br.close();
