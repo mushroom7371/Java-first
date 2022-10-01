e313baf19d0a2b0f35081be0929baad53ffcf5bd
juna desktop <wnsghk6670@naver.com>
2022-10-01 12:05:35 +0900

    백준 14645번 와이버스 부릉부릉

---
+++ src/BaekJoon/N14645.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.io.*;
+
+//와이버스 부릉부릉
+public class N14645 {
+
+    void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("비와이");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException{
+        new N14645().solution();
+    }
+}
