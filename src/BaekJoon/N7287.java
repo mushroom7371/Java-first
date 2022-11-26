a2e533134e3bd7c883a2181bbd4a3b58da30ed95
juna desktop <wnsghk6670@naver.com>
2022-11-26 14:49:01 +0900

    백준 7287번 등록

---
+++ src/BaekJoon/N7287.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.*;
+
+//등록
+public class N7287 {
+
+    void solution() throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("297\nwnsghk6670");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException{
+        new N7287().solution();
+    }
+}
