4b40263b0e9d0e3681b98f27cd901a6d65322aa0
junehwa <wnsghk6670@naver.com>
2022-11-28 13:21:40 +0900

    백준 11292번 키 큰 사람

--- src/BaekJoon/N11292.java
+++ src/BaekJoon/N11292.java
@@ -10,30 +10,22 @@ public class N11292 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCase;
 
-        while((testCase = Integer.parseInt(br.readLine())) != 0){
-            String[][] peopleInfo = new String[testCase][2];
+        while((testCase = Integer.parseInt(br.readLine())) != 0){   //입력으로 0이 들어오면 종료
+            String[][] peopleInfo = new String[testCase][]; //이름과 키를 담을 2차원배열을 선언
+            Double max = 0.0;   //키의 최대값을 저장할 변수
 
             for(int i = 0; i < testCase; i++){
-                peopleInfo[i] = br.readLine().split(" ");
-            }
-
-            Double max = Double.parseDouble(peopleInfo[0][1]);
-
-            for(int i = 0; i < testCase -1; i++){
-                Double tempA = Double.parseDouble(peopleInfo[i][1]);
-                Double tempB = Double.parseDouble(peopleInfo[i+1][1]);
-
-                if(Double.compare(tempA, tempB) >= 0){
-                    max = tempA;
-                }else{
-                    max = tempB;
+                peopleInfo[i] = br.readLine().split(" ");   //공백을 기준으로 문자열을 나눠 i차원배열이 가리키는 2차원 배열에 저장하도록 함
+                Double temp = Double.parseDouble(peopleInfo[i][1]); //입력으로 들어온 값 중 키를 임시변수로 담고
+                if(Double.compare(temp, max) >= 0){ //max값과 비교하여 max값을 설정
+                    max = temp;
                 }
             }
 
             for(int i = 0; i < testCase; i++){
-                Double temp = Double.parseDouble(peopleInfo[i][1]);
+                Double temp = Double.parseDouble(peopleInfo[i][1]); //키를 임시변수에 저장하고
 
-                if(Double.compare(max, temp) == 0){
+                if(Double.compare(max, temp) == 0){ //max값과 일치하면 버퍼에 기록
                     bw.write(peopleInfo[i][0] + " ");
                 }
             }
