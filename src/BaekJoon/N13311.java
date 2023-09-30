657a2b66a89ebc4abfa072f8a06e8407c73c159c
juna desktop <wnsghk6670@naver.com>
2023-09-30 10:21:24 +0900

    백준 13311번 행운의 편지

---
+++ src/BaekJoon/N13311.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.io.*;
+
+//행운의 편지
+public class N13311 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("-1");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N13311().solution();
+    }
+}
