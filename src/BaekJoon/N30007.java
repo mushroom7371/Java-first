fcce8ac4353c37dd28e83bf08a22bda4471608ae
juna desktop <wnsghk6670@naver.com>
2024-07-06 14:50:28 +0900

    백준 30007번 라면 공식

---
+++ src/BaekJoon/N30007.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//라면 공식
+public class N30007 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            int result = a * (x - 1) + b;
+            bw.write(result + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N30007().solution();
+    }
+}
