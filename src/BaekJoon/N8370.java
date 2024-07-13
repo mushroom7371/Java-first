e7f0f7f964cef865dd34c4c94994b37f3e9fa61c
juna desktop <wnsghk6670@naver.com>
2024-07-13 14:30:28 +0900

    백준 8370번 마이크로소프트 로고

---
+++ src/BaekJoon/N8370.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//Plane
+public class N8370 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int result = a * b + c * d;
+        bw.write(result + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N8370().solution();
+    }
+}
