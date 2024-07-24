6fa553dc58a23c7c86aee9dd11e5c76c756974b4
bigcraft juna <wnsghk6670@naver.com>
2024-07-24 17:44:07 +0900

    백준 15962번 새로운 시작

---
+++ src/BaekJoon/N15962.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.*;
+
+//새로운 시작
+public class N15962 {
+
+    void solution() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("파이팅!!");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N15962().solution();
+    }
+}
