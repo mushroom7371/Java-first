d80afe395c33245658cc2e027a3474f5b8a28415
juna desktop <wnsghk6670@naver.com>
2024-07-28 17:31:35 +0900

    백준 28444번 HI-ARC=?

---
+++ src/BaekJoon/N28444.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+//
+public class N28444 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int i = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int result = (h * i) - (a * r * c);
+
+        bw.write(String.valueOf(result));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N28444().solution();
+    }
+}
