35682c0b72c5139db15fcce725eceb1afe858973
bigcraft juna <wnsghk6670@naver.com>
2024-08-05 10:10:54 +0900

    백준 29738번 Goodbye, Code Jam

---
+++ src/BaekJoon/N29738.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.io.*;
+
+public class N29738 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= testCase; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n > 4500 && n <= 30000) {
+                bw.write("Case #" + i + ": Round 1\n");
+            } else if (n > 1000) {
+                bw.write("Case #" + i + ": Round 2\n");
+            } else if (n > 25){
+                bw.write("Case #" + i + ": Round 3\n");
+            } else {
+                bw.write("Case #" + i + ": World Finals\n");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N29738().solution();
+    }
+}
