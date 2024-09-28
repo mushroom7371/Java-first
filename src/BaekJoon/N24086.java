79c53f67e7bd273c1485a76b445531d05e52cd7a
juna desktop <wnsghk6670@naver.com>
2024-09-28 19:41:02 +0900

    백준 24086번 身長 (Height)

---
+++ src/BaekJoon/N24086.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.*;
+
+//身長 (Height)
+public class N24086 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int result = b - a;
+
+        bw.write(result + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N24086().solution();
+    }
+}
