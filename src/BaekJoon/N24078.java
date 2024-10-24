e82dd7c9dbe83198cd702e77de547caeb8b43303
bigcraft juna <wnsghk6670@naver.com>
2024-10-24 10:02:54 +0900

    백준 24078번 余り (Remainder)

---
+++ src/BaekJoon/N24078.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.*;
+
+//余り (Remainder)
+public class N24078 {
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int x = Integer.parseInt(br.readLine());
+        int result = x % 21;
+
+        bw.write(result + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new N24078().solution();
+    }
+}
